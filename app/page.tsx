3534db86615dd2b804aa087822a98360bdd13bb5
Madina <madinakrgl@gmail.com>
2024-12-14 21:51:47 +0500

    ci/cd test

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div>
       <h1 className="text-3xl text-center mt-6 font-bold text-gray-900">
-        Learning center
+        Learning center for math students
       </h1>
     </div>
   );
