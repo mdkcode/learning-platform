b9ca356ac08ff0fbc63a967c6c87d0fc40b1e862
Madina <madinakrgl@gmail.com>
2025-01-15 16:28:55 +0500

    feat(): next auth

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,7 +1,19 @@
+"use client";
+import { useSession } from "next-auth/react";
 import { MobileHeader } from "../ui/dashboard/MobileHeader";
 import SideNav from "../ui/dashboard/SideNav";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { Routes } from "../routes/routes";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
+  const router = useRouter();
+  const { data: session } = useSession();
+
+  useEffect(() => {
+    if (session?.expires && !session.user) router.push(Routes.HOME);
+  }, [session?.expires, session?.user, router]);
+
   return (
     <div className="flex flex-col md:h-screen md:flex-row">
       <MobileHeader />
--- app/ui/components/Button/NavButton.tsx
+++ app/ui/components/Button/NavButton.tsx
@@ -2,7 +2,7 @@
 import { Routes } from "@/app/routes/routes";
 import Button from "@/app/ui/components/Button/Button";
 import { ButtonType } from "@/app/ui/components/Button/utils";
-import { useRouter } from "next/navigation";
+import { signIn } from "next-auth/react";
 
 export const NavButton = ({
   className,
@@ -13,13 +13,18 @@ export const NavButton = ({
   buttonType?: ButtonType;
   link?: string;
 }) => {
-  const router = useRouter();
+  const handleSignIn = () => {
+    signIn("google", {
+      callbackUrl: link ?? Routes.DASHBOARD, // Redirect after successful login
+    });
+  };
+
   return (
     <Button
       label="Get started"
       buttonType={buttonType ?? ButtonType.NAVIGATION}
       className={className}
-      onClick={() => router.push(link ?? Routes.DASHBOARD)}
+      onClick={handleSignIn}
     />
   );
 };
